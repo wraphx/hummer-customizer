72c98bbff6bbd529ba4e88a7ab241cac73e4fb60
DrakBlack69 <tybyers21@gmail.com>
2024-05-31 12:22:02 -0400

    Final Changes

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
         <Canvas shadows camera={{ position: [0, 0, 20], fov:40 }}>
           <color attach="background" args={['grey']} />
           <Suspense fallback={null}>
-          <Hummer scale={0.5} position={[0, -1.1, 0]} />
+          <Hummer scale={2.3} position={[0, -1.1, 0]} />
           </Suspense>
           <hemisphereLight intensity={0.5} />
           <ContactShadows resolution={1024} frames={1} position={[0, -1.16, 0]} scale={15} blur={0.5} opacity={0.5} far={20} />
@@ -42,8 +42,8 @@ export default function App() {
             <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 6]} scale={[10, 1, 1]} />
             <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 9]} scale={[10, 1, 1]} />
             {/* Sides */}
-            <Lightformer intensity={10} rotation-y={Math.PI / 2} position={[-50, 2, 0]} scale={[100, 2, 1]} />
-            <Lightformer intensity={10} rotation-y={-Math.PI / 2} position={[50, 2, 0]} scale={[100, 2, 1]} />
+            <Lightformer intensity={25} rotation-y={Math.PI / 2} position={[-50, 2, 0]} scale={[100, 2, 1]} />
+            <Lightformer intensity={25} rotation-y={-Math.PI / 2} position={[50, 2, 0]} scale={[100, 2, 1]} />
             {/* Key */}
             <Lightformer form="ring" color="gray" intensity={10} scale={2} position={[10, 5, 10]} onUpdate={(self) => self.lookAt(0, 0, 0)} />
           </Environment>
---
+++ src/components/TexturePaths.jsx
@@ -0,0 +1,86 @@
+var TexturePaths = [
+    {
+        overlay: 1,
+        path: "textures/BaseMtl Base Color.png"
+    },
+    {
+        overlay: 2,
+        path: "textures/Slaughter.001 Base Color.png"
+    },
+    {
+        overlay: 3,
+        path: "textures/BatLady Base Color.001.png"
+    },
+    {
+        overlay: 4,
+        path: "textures/BrakeLine Base Color.001.png"
+    },
+    {
+        overlay: 5,
+        path: "textures/Danger Base Color.001.png"
+    },
+    {
+        overlay: 6,
+        path: "textures/Joker Base Color.001.png"
+    },
+    {
+        overlay: 7,
+        path: "textures/Madness Base Color.001.png"
+    },
+    {
+        overlay: 8,
+        path: "textures/Desert Base Color.001.png"
+    },
+    {
+        overlay: 9,
+        path: "textures/HotRods Base Color.001.png"
+    },
+    {
+        overlay: 10,
+        path: "textures/Mayan.001 Base Color.png"
+    },
+    {
+        overlay: 11,
+        path: "textures/SeaBreeze.001 Base Color.png"
+    },
+    {
+        overlay: 12,
+        path: "textures/ShapeShifter.001 Base Color.png"
+    },
+    {
+        overlay: 13,
+        path: "textures/Silk.001 Base Color.png"
+    },
+    {
+        overlay: 14,
+        path: "textures/SnakeSkin.001 Base Color.png"
+    },
+    {
+        overlay: 15,
+        path: "textures/Flare Base Color.001.png"
+    },
+    {
+        overlay: 16,
+        path: "textures/Dominator Base Color.001.png"
+    },
+    {
+        overlay: 17,
+        path: "textures/TopGun.001 Base Color.png"
+    },
+    {
+        overlay: 18,
+        path: "textures/Explosion Base Color.001.png"
+    },
+    {
+        overlay: 19,
+        path: "textures/WildWest.001 Base Color.png"
+    },
+    {
+        overlay: 20,
+        path: "textures/Zombie.001 Base Color.png"
+    },
+
+]
+
+
+export default TexturePaths
